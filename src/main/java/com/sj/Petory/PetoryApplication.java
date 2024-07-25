4b1ab2577060da0868763ce68086880472012ac0
soeun135 <soensuckun@naver.com>
2024-07-25 18:22:52 +0900

    [set] Project Initillaze setting

---
+++ src/main/java/com/sj/Petory/PetoryApplication.java
@@ -0,0 +1,13 @@
+package com.sj.Petory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sj/Petory/PetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sj.Petory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
