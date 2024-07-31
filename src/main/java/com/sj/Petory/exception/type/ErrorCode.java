5f5280f2a23843177cfbcc15830c5819d6027c93
soeun135 <soensuckun@naver.com>
2024-08-01 06:02:19 +0900

    [fix] fixed untracked files

--- src/main/java/com/sj/Petory/exception/type/ErrorCode.java
+++ src/main/java/com/sj/Petory/exception/type/ErrorCode.java
@@ -3,7 +3,6 @@ package com.sj.Petory.exception.type;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
