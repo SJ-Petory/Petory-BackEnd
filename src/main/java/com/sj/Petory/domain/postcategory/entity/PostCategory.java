41c87df89bd7034f83622a885b64766873508910
soeun135 <soensuckun@naver.com>
2024-08-16 02:14:44 +0900

    [refactor] 필요없는 의존성 제거

--- src/main/java/com/sj/Petory/domain/postcategory/entity/PostCategory.java
+++ src/main/java/com/sj/Petory/domain/postcategory/entity/PostCategory.java
@@ -1,16 +1,10 @@
 package com.sj.Petory.domain.postcategory.entity;
 
-import com.sj.Petory.domain.member.entity.Member;
 import com.sj.Petory.domain.post.entity.Post;
-import com.sj.Petory.domain.post.type.PostStatus;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
