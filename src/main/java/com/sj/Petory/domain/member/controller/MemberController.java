afc32e15c03de6d656f0f643e4413f33ad8aa763
soeun135 <soensuckun@naver.com>
2024-08-15 23:48:41 +0900

    [feat #53] 회원의 반려동물 목록 조회 API 구현

--- src/main/java/com/sj/Petory/domain/member/controller/MemberController.java
+++ src/main/java/com/sj/Petory/domain/member/controller/MemberController.java
@@ -1,16 +1,16 @@
 package com.sj.Petory.domain.member.controller;
 
-import com.sj.Petory.domain.member.dto.MemberAdapter;
-import com.sj.Petory.domain.member.dto.MemberInfoResponse;
-import com.sj.Petory.domain.member.dto.SignIn;
-import com.sj.Petory.domain.member.dto.SignUp;
+import com.sj.Petory.domain.member.dto.*;
 import com.sj.Petory.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/members")
@@ -56,6 +56,16 @@ public class MemberController {
             @AuthenticationPrincipal MemberAdapter memberAdapter) {
 
         return ResponseEntity.ok(memberService.getMembers(memberAdapter));
+    }
+
+    @GetMapping("/pets")
+    public ResponseEntity<Page<PetResponse>> getMembersPets(
+            @AuthenticationPrincipal MemberAdapter memberAdapter
+            , Pageable pageable) {
+
+        return ResponseEntity.ok(
+                memberService.getMembersPets(
+                        memberAdapter, pageable));
     }
 
     @GetMapping("/test")
