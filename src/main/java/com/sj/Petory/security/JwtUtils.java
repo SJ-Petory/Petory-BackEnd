1058d3e2ef0199efd132667121cceaac8151c225
soeun135 <soensuckun@naver.com>
2024-08-16 01:48:58 +0900

    [refactor] JWT 인증 로직 코드 리팩토링

--- src/main/java/com/sj/Petory/security/JwtUtils.java
+++ src/main/java/com/sj/Petory/security/JwtUtils.java
@@ -1,11 +1,9 @@
 package com.sj.Petory.security;
 
-import com.sj.Petory.domain.member.dto.MemberAdapter;
 import com.sj.Petory.domain.member.service.UserDetailsServiceImpl;
 import com.sj.Petory.exception.MemberException;
 import com.sj.Petory.exception.type.ErrorCode;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -78,13 +75,9 @@ public class JwtUtils {
 
     public boolean validateToken(String token) {
 
-        Date exp = Jwts.parser()
-                .setSigningKey(secretKey)
-                .parseClaimsJws(token)
-                .getBody()
-                .getExpiration();
+        Date exp = parseClaims(token).getExpiration();
 
-        if (exp.before(new Date())) {//날짜 개념 학습
+        if (exp.before(new Date())) {
             throw new MemberException(ErrorCode.TOKEN_EXPIRED);
         }
         return true;
@@ -100,6 +93,7 @@ public class JwtUtils {
                 , userDetails.getAuthorities()
         );
     }
+
     private String parseEmail(String token) {
         return parseClaims(token).getSubject();
     }
