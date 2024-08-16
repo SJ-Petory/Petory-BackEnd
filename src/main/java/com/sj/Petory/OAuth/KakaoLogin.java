f22a9c9f329f2ca3aa846611f207b91ce70c6751
soeun135 <soensuckun@naver.com>
2024-08-17 00:28:22 +0900

    [feat #64] 카카오 소셜로그인 구현

---
+++ src/main/java/com/sj/Petory/OAuth/KakaoLogin.java
@@ -0,0 +1,22 @@
+package com.sj.Petory.OAuth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class KakaoLogin {
+    private final KakaoLoginService kakaoLoginService;
+
+    @GetMapping("/oauth/kakao/callback")//인가코드 발급
+    public ResponseEntity<?> callbackKakao(@RequestParam("code") String code) {
+
+        System.out.println(code);
+        kakaoLoginService.getAccessTokenFromKakao(code);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
