9249eb5595baab985acb3f772f951992cba8ed1a
soeun135 <soensuckun@naver.com>
2024-08-22 19:16:08 +0900

    [fix #64] 리다이렉트 url로 받는 mapping post로 변경

--- src/main/java/com/sj/Petory/OAuth/KakaoLoginController.java
+++ src/main/java/com/sj/Petory/OAuth/KakaoLoginController.java
@@ -5,17 +5,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class KakaoLoginController {
     private final KakaoLoginService kakaoLoginService;
 
-    @GetMapping("/oauth/kakao/callback")//인가코드 발급
+    @PostMapping("/oauth/kakao/callback")//인가코드 발급
     public ResponseEntity<SignIn.Response> callbackKakao(
             @RequestParam("code") String code
             , @RequestBody @Valid ExtraUserInfo extraUserInfo) {
