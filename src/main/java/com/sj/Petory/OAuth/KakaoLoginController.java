555ecd762d7f251ec4d5abaa5fe4e19ff0c2c9c7
soeun135 <soensuckun@naver.com>
2024-08-23 03:09:35 +0900

    [fix #64] 카카오 서버에서 요청한 토큰을 프론트 페이지로 리다이렉트

--- src/main/java/com/sj/Petory/OAuth/KakaoLoginController.java
+++ src/main/java/com/sj/Petory/OAuth/KakaoLoginController.java
@@ -1,11 +1,14 @@
 package com.sj.Petory.OAuth;
 
 import com.sj.Petory.domain.member.dto.SignIn;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,12 +16,16 @@ public class KakaoLoginController {
     private final KakaoLoginService kakaoLoginService;
 
     @GetMapping("/oauth/kakao/callback")//인가코드 발급
-    public ResponseEntity<String> callbackKakao(
-            @RequestParam("code") String code) {
+    public ResponseEntity<Void> callbackKakao(
+            @RequestParam("code") String code
+            , HttpServletResponse response) throws IOException {
 
         System.out.println(code);
-        return ResponseEntity.ok(
-                kakaoLoginService.getAccessTokenFromKakao(code));
+        String accessTokenFromKakao = kakaoLoginService.getAccessTokenFromKakao(code);
+
+        String redirectUrl = "https://43.202.195.199/inputInfo?token=" + accessTokenFromKakao;
+
+        response.sendRedirect(redirectUrl);
     }
 
     @PostMapping("/oauth/kakao/extraInfo")
