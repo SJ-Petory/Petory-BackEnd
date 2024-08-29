c4bda3bbdf2c57b0cf243da03f716644852e750b
soeun135 <soensuckun@naver.com>
2024-08-29 18:36:55 +0900

    [fix #91] 요청 url 수정

--- src/main/java/com/sj/Petory/domain/friend/controller/FriendController.java
+++ src/main/java/com/sj/Petory/domain/friend/controller/FriendController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/friend")
+@RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendController {
 
