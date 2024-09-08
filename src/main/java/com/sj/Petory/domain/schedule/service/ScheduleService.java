f3569cdba4e03f1d951ba4fae5077fa41b23a4b9
soeun135 <soensuckun@naver.com>
2024-09-08 23:43:31 +0900

    [feat #121] 일정 목록 API 구현

--- src/main/java/com/sj/Petory/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/sj/Petory/domain/schedule/service/ScheduleService.java
@@ -8,6 +8,7 @@ import com.sj.Petory.domain.pet.repository.CareGiverRepository;
 import com.sj.Petory.domain.pet.repository.PetRepository;
 import com.sj.Petory.domain.schedule.dto.CreateCategoryRequest;
 import com.sj.Petory.domain.schedule.dto.CreateScheduleRequest;
+import com.sj.Petory.domain.schedule.dto.ScheduleListResponse;
 import com.sj.Petory.domain.schedule.entity.CustomRepeatPattern;
 import com.sj.Petory.domain.schedule.entity.PetSchedule;
 import com.sj.Petory.domain.schedule.entity.Schedule;
@@ -22,7 +23,11 @@ import com.sj.Petory.exception.PetException;
 import com.sj.Petory.exception.ScheduleException;
 import com.sj.Petory.exception.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -99,5 +104,27 @@ public class ScheduleService {
     private Pet getPetById(long petId) {
         return petRepository.findById(petId)
                 .orElseThrow(() -> new PetException(ErrorCode.PET_NOT_FOUND));
+    }
+
+    public Page<ScheduleListResponse> scheduleList(
+            final MemberAdapter memberAdapter, final Pageable pageable) {
+
+        Member member = getMember(memberAdapter);
+
+        //로그인 한 사용자가 만든 일정
+        Page<ScheduleListResponse> scheduleListResponses =
+                scheduleRepository.findByMember(member, pageable)
+                        .map(Schedule::toDto);
+
+        //로그인 한 사용자가 돌보미로 등록된 일정
+        // 1. 돌보미에서 로그인한 사용자가 돌보는 동물을 찾고(돌보미 테이블)
+        // 2. 해당 반려동물들로 일정을 찾고(반려동물 일정테이블)
+        // 3. dto 타입으로 반환
+
+        return careGiverRepository.findByMember(member, pageable)
+                .map(careGiver ->
+                        (ScheduleListResponse) petScheduleRepository.findByPet(careGiver.getPet())
+                                .stream().map(PetSchedule::toDto)
+                );
     }
 }
