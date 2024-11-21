ba53f9c9d7184e898418001fc152992ad04c818e
soeun135 <soensuckun@naver.com>
2024-11-21 22:43:31 +0900

    [feat] 배포서버 es 관련 설정

--- src/main/java/com/sj/Petory/common/es/MemberEsRepository.java
+++ src/main/java/com/sj/Petory/common/es/MemberEsRepository.java
@@ -3,9 +3,9 @@ package com.sj.Petory.common.es;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@Repository
+@EnableElasticsearchRepositories
 public interface MemberEsRepository extends ElasticsearchRepository<MemberDocument, Long> {
     Page<MemberDocument> findByNameOrEmail(String name, String email, Pageable pageable);
 }
