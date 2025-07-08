c4134849c5c517c59807b3b39a845c1e032a1228
soeun135 <soensuckun@naver.com>
2025-07-08 17:21:35 +0900

    [feat #190] 게시글 업로드 구현에 필요한 여러 이미지 받는 로직 구현

--- src/main/java/com/sj/Petory/domain/post/dto/CreatePostRequest.java
+++ src/main/java/com/sj/Petory/domain/post/dto/CreatePostRequest.java
@@ -1,6 +1,9 @@
 package com.sj.Petory.domain.post.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,5 +15,5 @@ public class CreatePostRequest {
     private Long categoryId;
     private String title;
     private String content;
-    private String imageUrl;
+    private List<MultipartFile> image;
 }
