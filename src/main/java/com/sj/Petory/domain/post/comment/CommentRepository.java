77ce2dbe6857ef6e4319951cfdabe84d0fd75d6e
soeun135 <soensuckun@naver.com>
2025-09-09 03:32:11 +0900

    [feat #211] 댓글 수정  API 구현

--- src/main/java/com/sj/Petory/domain/post/comment/CommentRepository.java
+++ src/main/java/com/sj/Petory/domain/post/comment/CommentRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     long countAllByPostAndStatus(Post post, CommentStatus commentStatus);
+
+    Optional<Comment> findByCommentIdAndStatus(long commentId, CommentStatus commentStatus);
 }
