fe629217cd907dc3aea92082961f22d467d5ce0d
SONI <soensuckun@naver.com>
2026-01-04 00:18:16 +0900

    [fix] cors 오류 해결을 위한 orgin 수정

--- src/main/java/com/sj/Petory/config/WebConfig.java
+++ src/main/java/com/sj/Petory/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://petory.site")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "PATCH")
+                        .allowedOrigins("https://www.petory.site")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
