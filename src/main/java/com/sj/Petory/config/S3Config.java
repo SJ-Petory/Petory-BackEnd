3ee1f1029aefee75cafd492cde97ad448f3a5076
SONI <soensuckun@naver.com>
2026-02-07 01:53:33 +0900

    [Feat] 이미지 저장소 MINIO로 변경에 따른 설정

--- src/main/java/com/sj/Petory/config/S3Config.java
+++ src/main/java/com/sj/Petory/config/S3Config.java
@@ -3,6 +3,7 @@ package com.sj.Petory.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,13 +22,17 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Value("${cloud.aws.s3.endpoint}")
+    private String endpoint;
+
     @Bean
     public AmazonS3 amazonS3() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .withPathStyleAccessEnabled(true)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
                 .build();
     }
 }
